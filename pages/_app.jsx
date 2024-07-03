536fda282212b88c1986bc4396337ef17ca201da
mercurialli <ninoklala2@gmail.com>
2024-07-03 12:28:54 +0300

    delete div

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,5 @@
 import "../styles/global.css";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <div>
-      <Component {...pageProps} />
-    </div>
-  );
+  return <Component {...pageProps} />;
 }
